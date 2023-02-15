228f6dae2256d391c73b03c4ac47b3712ec26493
FAyllan101 <103951910+FAyllan101@users.noreply.github.com>
2023-02-15 11:40:41 -0300

    fundamentos js

---
+++ Aulas Js/Fundamentos/ternario.js
@@ -0,0 +1,7 @@
+// primeira parte operador relacional(com verdadeiro ou falso) ? depois condição veradeiro : falso 
+const resultado = nota => nota >= 7 ? 'aprovado' : 'reprovado'
+
+console.log(resultado(7.1))
+console.log(resultado(6.9))
+console.log(resultado(7))
+
