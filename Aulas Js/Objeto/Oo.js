c9fc62b5b5d029c3c53d234e894d0d998a3803e3
FAyllan101 <103951910+FAyllan101@users.noreply.github.com>
2023-02-27 17:08:20 -0300

    .

---
+++ Aulas Js/Objeto/Oo.js
@@ -0,0 +1,19 @@
+//codigo não executavel
+
+//procedural 
+//um trecho que volta para ser ultilizado novamente na aplicação
+processamento(valor1,valor2,valor3)
+
+//oo=> objetos com atributos e funcoes
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() // foco passou para objeto 
+
+//
